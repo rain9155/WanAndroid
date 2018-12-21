3c1704e89092c7c7d487aad2dcf2616857e83421
rain9155 <jianyu9155@gmail.com>
2018-12-21 17:52:01 +0800

    实现懒加载

--- app/src/main/java/com/example/hy/wanandroid/base/fragment/AbstractLazyLoadFragment.java
+++ app/src/main/java/com/example/hy/wanandroid/base/fragment/AbstractLazyLoadFragment.java
@@ -11,12 +11,14 @@ import androidx.fragment.app.Fragment;
 
 
 /**
+ * 实现懒加载
  * Create by 陈健宇 at 2018/7/15
  */
 public abstract class AbstractLazyLoadFragment extends Fragment {
 
     private boolean isViewCreated = false;//布局是否被创建
     private boolean isLoadData = false;//数据是否加载
+    private boolean isFirstVisible = true;//是否第一次可见
     private String TAG = "baselazy";
 
     @Override
@@ -27,20 +29,47 @@ public abstract class AbstractLazyLoadFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        LogUtil.d(TAG, "onActivityCreated: " + String.valueOf(this.isHidden()));
-        if(isViewCreated && getUserVisibleHint()){
-            onLazyLoadData();
-            isLoadData = true;
+        if(isFragmentVisible(this) && this.isAdded()){
+            if ((this.getParentFragment() != null && isFragmentVisible(this.getParentFragment()))
+                    || this.getParentFragment() == null) {
+                onLazyLoadData();
+                isLoadData = true;
+                if(isFirstVisible)
+                    isFirstVisible = false;
+            }
         }
     }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-        LogUtil.d(TAG, "setUserVisibleHint: " + String.valueOf(this.isHidden()));
-        if(isVisibleToUser && isViewCreated && !isLoadData){
+        LogUtil.d(TAG, "setUserVisibleHint(): "
+                + " hide: " + this.isHidden()
+                + " add :" + this.isAdded()
+                + " visible: " + this.isVisible()
+                + " resumed: " + this.isResumed());
+        if(isFragmentVisible(this) && !isLoadData && isViewCreated && this.isAdded()){
             onLazyLoadData();
             isLoadData = true;
+        }
+    }
+
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        LogUtil.d(TAG, "onHiddenChanged(): "
+                + " hide: " + this.isHidden()
+                + " add :" + this.isAdded()
+                + " visible: " + this.isVisible()
+                + " resumed: " + this.isResumed());
+        //onHiddenChanged调用在Resumed之前，所以此时可能，fragment被add, 但还没resumed
+        if(!hidden && !this.isResumed())
+            return;
+        //使用hide和show时，fragment的所有生命周期方法都不会调用，除了onHiddenChanged（）
+        if(!hidden && isFirstVisible && this.isAdded()){
+            onLazyLoadData();
+            isFirstVisible = false;
         }
     }
 
@@ -51,10 +80,13 @@ public abstract class AbstractLazyLoadFragment extends Fragment {
         super.onDestroyView();
         isViewCreated = false;
         isLoadData = false;
+        isFirstVisible = true;
     }
 
-    @Override
-    public void onHiddenChanged(boolean hidden) {
-        super.onHiddenChanged(hidden);
+    /**
+     * 当前Fragment是否可见
+     */
+    private boolean isFragmentVisible(Fragment fragment) {
+        return !fragment.isHidden() && fragment.getUserVisibleHint();
     }
 }
