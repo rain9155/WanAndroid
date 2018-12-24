7159e76e9232966395ff731d6a2551506d84247f
rain9155 <jianyu9155@gmail.com>
2018-12-24 21:16:34 +0800

    fix a bug

--- app/src/main/java/com/example/hy/wanandroid/view/hierarchy/HierarchySecondFragment.java
+++ app/src/main/java/com/example/hy/wanandroid/view/hierarchy/HierarchySecondFragment.java
@@ -80,7 +80,9 @@ public class HierarchySecondFragment extends BaseLoadFragment implements Hierarc
             isLoadMore = true;
         });
         mArticlesAdapter.setOnItemClickListener((adapter, view, position) -> {//跳转文章
+            mArticlePosition = position;
             Article article = mArticleList.get(position);
+            mArticle = article;
             ArticleActivity.startActicityForResultByFragment(mActivity, this, article.getLink(), article.getTitle(), article.getId(), article.isCollect(), false, Constant.REQUEST_REFRESH_ARTICLE);
         });
         mArticlesAdapter.setOnItemChildClickListener((adapter, view, position) -> {//收藏
