f04a95f3f0fc81b27a50974652c822ab38bf2b7a
rain9155 <jianyu9155@gmail.com>
2019-02-23 13:00:09 +0800

    准备添加单元测试

---
+++ app/src/test/java/com/example/hy/wanandroid/presenter/MainPresenterTest.java
@@ -0,0 +1,34 @@
+package com.example.hy.wanandroid.presenter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 陈健宇 at 2019/2/22
+ */
+public class MainPresenterTest {
+
+    private MainPresenter mMainPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void subscribleEvent() {
+    }
+
+    @Test
+    public void setCurrentItem() {
+    }
+
+    @Test
+    public void getCurrentItem() {
+    }
+
+    @Test
+    public void checkVersion() {
+    }
+}
