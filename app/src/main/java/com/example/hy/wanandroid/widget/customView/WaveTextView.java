6fadae71219513ae16cbd3dcd345199d6434087b
rain9155 <jianyu9155@gmail.com>
2019-07-25 21:14:53 +0800

    添加启动动画，会抖动的WanAndroid

--- app/src/main/java/com/example/hy/wanandroid/widget/customView/WaveTextView.java
+++ app/src/main/java/com/example/hy/wanandroid/widget/customView/WaveTextView.java
@@ -8,14 +8,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
 import androidx.annotation.Nullable;
 
+import com.example.commonlib.utils.LogUtil;
 import com.example.hy.wanandroid.R;
+
 
 /**
  * 会抖动的字
@@ -23,17 +27,23 @@ import com.example.hy.wanandroid.R;
  */
 public class WaveTextView extends View {
 
-    private static final int A = 200;
+    private static final String TAG = WaveTextView.class.getSimpleName();
+    private static final int SAMPLING_SIZE = 200;//屏幕上采样点的个数，数值越大越精确
 
     private String mText;
     private float mTextSize;
     private int mTextColor;
+    private int isBold;
 
     private Paint mTextPaint;
     private Path mTextPath;
     private ValueAnimator mValueAnimator;
-    private float mTextLen;
-    private float a, m;
+    private int mTextHeight;
+    private int mTextWidth;
+    private int mTextCenter;
+    private float a, φ, A = 100;
+    private float[] mSamplingX = new float[SAMPLING_SIZE + 1];//采样点坐标
+    private float[] mMapX = new float[SAMPLING_SIZE + 1];//把采样点坐标映射到[-2, 2]范围的坐标
 
     public WaveTextView(Context context) {
         super(context);
@@ -51,46 +61,60 @@ public class WaveTextView extends View {
     }
 
     private void init(Context context, AttributeSet attrs) {
-
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.WaveTextView);
         mText = typedArray.getString(R.styleable.WaveTextView_text);
-        mTextSize = typedArray.getDimension(R.styleable.WaveTextView_size, R.dimen.sp_26);
+        mTextSize = typedArray.getDimension(R.styleable.WaveTextView_size, 26);
         mTextColor = typedArray.getColor(R.styleable.WaveTextView_color, Color.BLACK);
+        A = typedArray.getInteger(R.styleable.WaveTextView_amplitude, 100);
+        isBold = typedArray.getInteger(R.styleable.WaveTextView_style, 0);
         typedArray.recycle();
 
-        mTextPaint = new Paint();
+        mTextPaint = new TextPaint();
         mTextPaint.setAntiAlias(true);
         mTextPaint.setTextSize(mTextSize);
         mTextPaint.setColor(mTextColor);
-        mTextPaint.setStrokeWidth(1);
-
+        if(isBold == 1) mTextPaint.setFakeBoldText(true);
+        //mTextPaint.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
         mTextPath = new Path();
+        mTextWidth = (int) mTextPaint.measureText(mText);
+        Paint.FontMetricsInt fontMetrics = mTextPaint.getFontMetricsInt();
+        mTextHeight = fontMetrics.bottom - fontMetrics.top;
+        Rect rect = new Rect();
+        mTextPaint.getTextBounds(mText, 0, mText.length(), rect);
+        mTextCenter = rect.height() / 2;
 
-        //正弦函数的一个周期
+        //φ的取值范围为正弦函数的一个周期：2π
         mValueAnimator = ValueAnimator.ofFloat(0, (float) (2 * Math.PI));
         mValueAnimator.setInterpolator(new LinearInterpolator());
         mValueAnimator.addUpdateListener(animation -> {
             float progress = (float) animation.getAnimatedValue();
-            m = progress;
+            φ = progress;
             a = (float) (1 - progress / (2 * Math.PI)) * A;
-            invalidate();
+            LogUtil.d(TAG, "addUpdateListener(), a = " + a + ", φ = " + φ);
+            postInvalidate();
         });
-        mValueAnimator.setDuration(1000);
+        mValueAnimator.setDuration(2500);
+        postDelayed(() -> mValueAnimator.start(), 100);
 
-        mTextLen = mTextPaint.measureText(mText);
+        for(int i = 0; i <= SAMPLING_SIZE; i++) {
+            float gap = (float) mTextWidth / SAMPLING_SIZE;
+            mSamplingX[i] = i * gap;
+            mMapX[i] = mSamplingX[i] / (float) mTextWidth * 4 - 2;
+        }
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int measureWidthMode = MeasureSpec.getMode(widthMeasureSpec);
         int measureHeightMode = MeasureSpec.getMode(heightMeasureSpec);
-        int measureWidth = MeasureSpec.getSize(measureWidthMode);
-        int measureHeight = MeasureSpec.getSize(measureHeightMode);
+        int measureWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int measureHeight = MeasureSpec.getSize(heightMeasureSpec);
         //文字的默认宽高
-        int defaultWidth, defaultHeight;
-        defaultWidth = (int) mTextLen;
-        Paint.FontMetricsInt fontMetrics = mTextPaint.getFontMetricsInt();
-        defaultHeight = fontMetrics.bottom - fontMetrics.top;
+        int defaultHeight, defaultWidth;
+        defaultHeight = (int) (A * 2);
+        defaultWidth = mTextWidth;
+        if(measureHeight < defaultHeight) measureHeight = defaultHeight;
+        if(measureWidth < defaultWidth) measureWidth = defaultWidth;
         setMeasuredDimension(
                 measureWidthMode == MeasureSpec.EXACTLY ? measureWidth : defaultWidth,
                 measureHeightMode == MeasureSpec.EXACTLY ? measureHeight : defaultHeight
@@ -99,22 +123,46 @@ public class WaveTextView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-
+        //让字体在中间
+        canvas.translate((float) (getWidth() - mTextWidth) / 2, (float) getHeight() / 2 +  mTextCenter);
         mTextPath.reset();
+        LogUtil.d(TAG, "onDraw(), width = " + canvas.getWidth() + ", mTextWidth = " + mTextWidth);
+        for(int i = 0; i <= SAMPLING_SIZE; i++){
+            float y = calculateY(mMapX[i]);
+            float x = mSamplingX[i];
+            LogUtil.d(TAG, "onDraw() "
+                    + ", samplingX = " + mSamplingX[i]
+                    + ", mapX = " + mMapX[i]
+                    + ", x = " + x
+                    + ", y = " + y);
+            if(i == 0){
+                mTextPath.moveTo(x, y);
+            }else {
+                mTextPath.lineTo(x, y);
+            }
+        }
+        canvas.drawTextOnPath(mText, mTextPath, 0, 0, mTextPaint);
+    }
 
-
+    @Override
+    protected void onDetachedFromWindow() {
+        mValueAnimator.cancel();
+        mValueAnimator = null;
+        super.onDetachedFromWindow();
     }
 
     /**
-     * 计算 y 轴值: y = A * sin(w * x + m) + k，这里k = 0
-     * A：管理 正弦函数 的振幅，即上下摆动的幅度
-     * w：管理 正弦函数 的水平收缩幅度
-     * m：管理 正弦函数 的水平偏移量
-     * k：管理 正弦函数 的垂直偏移量
+     * 计算 y 轴值: y = A * sin(ω * x + φ) + k
+     * A： 表示振幅，对应正弦曲线的最大高度
+     * ω：表示角速度，对应正弦曲线的水平收缩幅度，换成频率是 2πf
+     * φ：表示初始相位，对应正弦曲线初始在 x 轴的偏移量
+     * k： 表示垂直偏距，对应正弦曲线初始在 y 轴上的偏移量
+     * 这里是：y = A * (4 / (4 + x^4))^2.5 * sin(0.75πx)
      */
     private float calculateY(float x) {
-        double b = Math.pow(4 / (4 + Math.pow(4 * x / mTextLen, 4)), 2.5f) * a;
-        return (float) (b * Math.sin(Math.PI * x / 200 - m));
+        //b为衰减函数 b = (4 / (4 + x^4))^2.5
+        double b = Math.pow(4 / (4 + Math.pow(x, 4)), 2.5f);
+        return (float) (a * b * Math.sin(0.75f * Math.PI * x - φ));
     }
 
 
